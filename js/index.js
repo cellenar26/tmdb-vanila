3c1fd726ffcfafd7503106f226c1f6c89f32cdf9
cellenar26 <cellenar26@gmail.com>
2021-09-27 20:13:09 +0900

    done loading component

--- js/index.js
+++ js/index.js
@@ -1,31 +1,25 @@
-import { getNowPlaying, getUpcomingMovies } from './api.js'
-import { drawNowPlaying, drawUpcomingMovies } from './draw.js';
+import { getNowPlaying, getUpcomingMovies } from "./api.js";
+import { drawNowPlaying, drawUpcomingMovies } from "./draw.js";
 
-const test = document.querySelector('.test')
-const ac = document.querySelector('.article-container')
+const loader = document.querySelector(".loader");
+const articleContainer = document.querySelector(".article-container");
 
-
-
- const  init = async()=> {
-     test.style.backgroundColor = 'white'
-     test.style.zIndex = 15
-     test.style.width = "100%"
-    //  test.style.height = "100%"
-    // ac.style.display = 'none'
-    
-    console.log(test)
-    const nowPlaying = await getNowPlaying()
-    const upcomingMovies = await getUpcomingMovies()
-    
-    drawNowPlaying(nowPlaying.results)
-    drawUpcomingMovies(upcomingMovies.results)
-}
-init();
+const init = async () => {
+  const nowPlaying = await getNowPlaying();
+  const upcomingMovies = await getUpcomingMovies();
+  setTimeout(loading(), 1000);
+  drawNowPlaying(nowPlaying.results);
+  drawUpcomingMovies(upcomingMovies.results);
+};
 
 const loading = () => {
-    //  로딩 창을 띄운다
-    //  1초에 전역 카운트를 1씩 증가 시키는 함수를 호출한다.
-    // 그 전역 카운트가 3이 되면 clear 실시
- 
-    
-}
+  loader.style.display = "none";
+};
+
+window.addEventListener("beforeunload", (event) => {
+  event.preventDefault();
+  console.log("unload");
+  articleContainer.style.display = "none";
+});
+
+init();
