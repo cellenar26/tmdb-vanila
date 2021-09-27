7aeaffc50eb2250a01013197eb4bd9cc98dd3fc7
cellenar26 <cellenar26@gmail.com>
2021-09-27 16:59:12 +0900

    making loading component

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 import { getNowPlaying, getUpcomingMovies } from './api.js'
 import { drawNowPlaying, drawUpcomingMovies } from './draw.js';
 
+const test = document.querySelector('.test')
+const ac = document.querySelector('.article-container')
+
+
+
  const  init = async()=> {
+     test.style.backgroundColor = 'white'
+     test.style.zIndex = 15
+     test.style.width = "100%"
+    //  test.style.height = "100%"
+    // ac.style.display = 'none'
+    
+    console.log(test)
     const nowPlaying = await getNowPlaying()
     const upcomingMovies = await getUpcomingMovies()
     
@@ -9,3 +21,11 @@ import { drawNowPlaying, drawUpcomingMovies } from './draw.js';
     drawUpcomingMovies(upcomingMovies.results)
 }
 init();
+
+const loading = () => {
+    //  로딩 창을 띄운다
+    //  1초에 전역 카운트를 1씩 증가 시키는 함수를 호출한다.
+    // 그 전역 카운트가 3이 되면 clear 실시
+ 
+    
+}
